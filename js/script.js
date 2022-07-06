dd2e31b5147c4355257f6cc6bd232e4c6d07858b
MrGians <gianlumura97@gmail.com>
2022-07-06 15:37:03 +0200

    Milestone 2 Done

--- js/script.js
+++ js/script.js
@@ -24,6 +24,8 @@
 //  3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 
 
+
+// Definisco l'array di oggetti con i relativi Post
 const socialPostList = [
   {
     post_id: 1,
@@ -53,3 +55,54 @@ const socialPostList = [
     post_likes: 29,
   },
 ];
+
+
+// Dichiaro variabili DOM
+const postContainer = document.getElementById("container");
+
+// Inizializzo la variabile di appoggio postModule che conterrà i vari post da stampare sul DOM
+let postModule = "";
+
+// Creo un ciclo FOR per girare in tutti gli Oggetti dell'array
+for (let i = 0; i < socialPostList.length; i++) {
+
+  // Eseguo il Destructuring dell'array di oggetti
+  const {post_id, author_name, author_pic, post_date, post_content, post_image, post_likes} = socialPostList[i];
+
+  // Creo il post da stampare in pagina tramite il template literal
+  postModule += `
+    <div class="post">
+      <div class="post__header">
+        <div class="post-meta">
+          <div class="post-meta__icon">
+            <img class="profile-pic" src="${author_pic}" alt="${author_name}" />
+          </div>
+          <div class="post-meta__data">
+            <div class="post-meta__author">${author_name}</div>
+            <div class="post-meta__time">${post_date}</div>
+          </div>
+        </div>
+      </div>
+      <div class="post__text">${post_content}</div>
+      <div class="post__image">
+        <img src="${post_image}" alt="landscape ${post_id}" />
+      </div>
+      <div class="post__footer">
+        <div class="likes js-likes">
+          <div class="likes__cta">
+            <a class="like-button js-like-button" href="#" data-postid="${post_id}">
+              <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+              <span class="like-button__label">Mi Piace</span>
+            </a>
+          </div>
+          <div class="likes__counter">Piace a <b id="like-counter-${post_id}" class="js-likes-counter">${post_likes}</b> persone</div>
+        </div>
+      </div>
+    </div>
+  `;
+}
+// Inserisco nel DOM i post generati dal ciclo
+postContainer.innerHTML = postModule;
+
+
+
